dd2367a2b776f619f3823212e510d741debd3801
NicolasRendy <nicolasrendy63@gmail.com>
2024-11-11 18:58:06 +0700

    Memperbaiki Login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\User;
@@ -12,8 +11,6 @@ class UserController extends Controller
 {
     public function registerUser(Request $request) //:string //RedirectResponse
     {
-
-        //Validasi input dari form
         $data = [
             'nama' => $request->input('nama'),
             'alamat' => $request->input('alamat'),
@@ -24,42 +21,15 @@ class UserController extends Controller
 
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $image = $request->file('image');
-            // Get the binary content of the image
             $data['fotoKTP'] = file_get_contents($image->getRealPath());
         } else {
-            // Default to null if no image is provided or if invalid
             $data['fotoKTP'] = null;
         }
 
-        // Save the user with the image data in the database
         $user = User::create($data);
 
-        // Redirect or return a success view
         return redirect('/layanan');
     }
-
-    // public function loginProses(Request $request)
-    // {
-
-    //     $data = [
-    //         'email' => $request->input('email'),
-    //         'password' => $request->input('password')
-    //     ];
-
-    //     $user = User::select('email', 'password')
-    //         ->where('email', $data['email'])
-    //         ->first();
-
-    //     if($user['password'] == $data['password']){
-    //         return redirect('/layanan');
-    //     }
-
-
-    //     // // Login gagal
-    //     // return back()->withErrors([
-    //     //     'email' => 'Email atau password salah.',
-    //     // ])->onlyInput('email');
-    // }
 
     
     public function loginProses(Request $request)
@@ -70,6 +40,8 @@ class UserController extends Controller
         ];
 
         $user = User::where('email', $data['email'])->first();
+
+        
     
         if ($user && Hash::check($data['password'], $user->password)) {
             // Jika login berhasil
@@ -77,9 +49,5 @@ class UserController extends Controller
             return redirect('/layanan');
         }
     
-        // Jika login gagal
-        return back()->withErrors([
-            'email' => 'Email atau password salah.',
-        ])->onlyInput('email');
     }
 }
