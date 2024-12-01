0601125989dc30ae46b50cc7c0cc54298f2462f2
NicolasRendy <nicolasrendy63@gmail.com>
2024-12-01 13:54:31 +0700

    test main branch

--- routes/web.php
+++ routes/web.php
@@ -84,7 +84,7 @@ Route::post('/menyewaProses', [MenyewaController::class, 'store']);
 
 // Route::get('/daftarPenyewaan',function(){
 //     return view('daftarPenyewaan');
-// })->middleware('auth');
+// })->middleware('auth'); // test bracnh
 
 // test
 Route::get('/motors', [MotorController::class, 'index'])->name('motors.index');
