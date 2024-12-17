71ac8893a19333bc454e53f9494016871238493c
Miadella <miadella.diaswara@gmail.com>
2024-12-17 13:04:30 +0700

    Perubahan Home dan Help

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,11 +1,11 @@
 <html lang="id">
 <!DOCTYPE html>
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="{{ asset('css/styleInclude.css') }}">
-<title>Rental Motor Tunas Baru</title>
-<style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/styleInclude.css') }}">
+    <title>Rental Motor Tunas Baru</title>
+    <style>
        .container {
            width: 80%;
            margin: 20px auto;
@@ -47,31 +47,31 @@
 </head>
 
 <body>
-<header>
-<nav>
-            <a href="/home" class="nav-button">Home</a>
+    <header>
+        <nav>
             <a href="/home" class="nav-button active">Home</a>
-<a href="/layanan" class="nav-button">Melihat Layanan</a>
-<a href="/login" class="nav-button">Login</a>
-<a href="/Registrasi" class="nav-button">Registrasi</a>
-</nav>
-</header>
+            <a href="/layanan" class="nav-button">Melihat Layanan</a>
+            <a href="/login" class="nav-button">Login</a>
+            <a href="/Registrasi" class="nav-button">Registrasi</a>
+        </nav>
+    </header>
 <br><br>
 
-<div class="container">
-<div class="content">
-<h2>Selamat Datang di Rental Motor Tunas Baru</h2>
-<p>Kami menyediakan layanan rental motor dengan berbagai pilihan motor terbaru dan berkualitas. Nikmati
-perjalanan Anda dengan motor kami yang terawat dengan baik dan harga sewa yang terjangkau.</p>
-<div class="gallery">
-<img src="{{ asset('images/home.jpg') }}" alt="Motor 1" class="motor-image">
-</div>
-</div>
-</div>
+    <div class="container">
+    <div class="content">
+        <h2>Selamat Datang di Rental Motor Tunas Baru</h2>
+        <p>Kami menyediakan layanan rental motor dengan berbagai pilihan motor terbaru dan berkualitas. Nikmati
+            perjalanan Anda dengan motor kami yang terawat dengan baik dan harga sewa yang terjangkau.</p>
+    <div class="gallery">
+    <img src="{{ asset('images/home.jpg') }}" alt="Motor 1" class="motor-image">
+    </div>
+    </div>
+    </div>
 <br><br><br><br><br><br>
-<footer>
-<p>Hubungi kami: 081-233-689 | email@TunasBaru.com</p>
-</footer>
+    <footer>
+        <p>Hubungi kami: 081-233-689 | email@TunasBaru.com</p>
+    </footer>
 
 
 </body>
+</html>
