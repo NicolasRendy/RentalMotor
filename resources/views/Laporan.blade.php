f437f781a4fec5b068201a9446ab7dd62d67bcd1
Miadella <miadella.diaswara@gmail.com>
2024-12-17 13:46:24 +0700

    Tambah Laporan Keuangan

---
+++ resources/views/Laporan.blade.php
@@ -0,0 +1,35 @@
+<html lang="id">
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/styleInclude.css') }}">
+    <title>Layanan Rental Motor Tunas Baru</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <style>
+
+    </style>
+
+<body>
+    <header>
+        <nav>
+            <a href="/Laporan" class="nav-button active">Laporan Keuangan</a>
+            <a href="/kelola" class="nav-button">Kelola</a>
+            <a href="/Konfirmasi" class="nav-button">Konfirmasi</a>
+            <div class="dropdown">
+                <a href="maintenance" class="nav-button">Maintenance</a>
+                <div class="dropdown-content">
+                    <button onclick="window.location.href='/input'">Input Jadwal</button>
+                    <button onclick="window.location.href='/lihat'">Lihat Jadwal</button>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <br><br><br><br><br><br>
+    <footer>
+        <p>Hubungi kami: 081-233-689 | email@TunasBaru.com</p>
+    </footer>
+</body>
+
+</html>
